91793cb0bddda8cb6278cbc7325bc0db0228e92f
iDave7 <56477312+iDave7@users.noreply.github.com>
2023-07-21 19:44:00 +0200

    Revisión 20230721

--- matriz_sumas.py
+++ matriz_sumas.py
@@ -12,14 +12,17 @@ def imprimir_matriz(matriz):
     # Imprime la matriz generada en pantalla con las cabeceras de filas y columnas.
     
     N = len(matriz)
+   
     # Imprime cabecera de columnas
+   
     print("   ", end="")
     for i in range(1, N + 1):
         print(f"C{i}", end=" ")
     print()
 
-    for i, fila in enumerate(matriz):
-        # Imprime cabecera de filas y valores de fila
+    # Imprime cabecera de filas y valores de fila
+
+    for i, fila in enumerate(matriz):   
         print(f"F{i+1}", end=" ")
         for num in fila:
             print(f"{num:2}", end=" ")
@@ -45,52 +48,32 @@ def imprimir_sumas(filas_suma, columnas_suma):
     for i, suma in enumerate(columnas_suma):
         print(f"C{i+1}: {suma}")
 
-# def main():
-
-#     # Función principal para la obtención del input del usuario para el valor de N
-
-#     try:
-#         N = float(input("Ingrese un número entero positivo N para el tamaño de la matriz: "))
-        
-#         # Comprobación de que N es entero positivo
-#         if N <= 0 or N != int(N):
-#             raise ValueError("El número N debe ser un entero positivo.")
-        
-#     except ValueError as e:
-#         print(f"Error: {e}")
-#         return
-
-#     matriz = generar_matriz(int(N))
-#     print("Matriz generada:")
-#     imprimir_matriz(matriz)
-
-#     filas_suma, columnas_suma = calcular_sumas(matriz)
-#     imprimir_sumas(filas_suma, columnas_suma)
-
 def main():
     while True:
         try:
             N = input("Ingrese un número entero positivo N para el tamaño de la matriz: ")
 
             # Comprobación de que N es un número entero positivo
+            # e.d.: no carácter, no decimal, no negativo y no cero
+
             if N.isdigit() and int(N) > 0:
-                N = int(N)  # Convertir N a entero después de la comprobación
+                N = int(N)  # Convierte N a entero después de la comprobación
                 break
             else:
                 raise ValueError("El número N debe ser un entero positivo válido.")
         except ValueError as e:
             print(f"Error: {e}")
 
+    # Llamado a la función de impresión de matriz
+
     matriz = generar_matriz(N)
     print("Matriz generada:")
     imprimir_matriz(matriz)
 
+    # Cálculo e impresión de las sumas de filas y columnas.
+    
     filas_suma, columnas_suma = calcular_sumas(matriz)
     imprimir_sumas(filas_suma, columnas_suma)
-
-# Resto del código (generar_matriz, imprimir_matriz, calcular_sumas, imprimir_sumas, etc.) permanece sin cambios
-
-
 
 # Ejecutor del programa
 if __name__ == "__main__":
