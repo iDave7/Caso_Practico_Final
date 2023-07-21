51bd31dd329980bbd6be0ac9603982688d5d91c7
iDave7 <56477312+iDave7@users.noreply.github.com>
2023-07-21 19:47:11 +0200

    Revisión final 20230721

--- test_matriz_sumas.py
+++ test_matriz_sumas.py
@@ -4,23 +4,31 @@ from matriz_sumas import calcular_sumas
 
 class TestMatrizSumas(unittest.TestCase):
 
+    # Comprobación de la generación de una matriz 3x3
+
     def test_generar_matriz(self):
         matriz = generar_matriz(3)
         self.assertEqual(len(matriz), 3)
         for fila in matriz:
             self.assertEqual(len(fila), 3)
 
+    # Comprobación de la correcta ejecución de sumas de filas y columnas para una matriz 3x3 dada
+    
     def test_calcular_sumas(self):
         matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
         filas_suma, columnas_suma = calcular_sumas(matriz)
         self.assertEqual(filas_suma, [6, 15, 24])
         self.assertEqual(columnas_suma, [12, 15, 18])
 
+    # Comprobación de la correcta ejecución de sumas de filas y columnas para una matriz vacía
+    
     def test_calcular_sumas_matriz_vacia(self):
         matriz = []
         filas_suma, columnas_suma = calcular_sumas(matriz)
         self.assertEqual(filas_suma, [])
         self.assertEqual(columnas_suma, [])
+
+    # Comprobación de la correcta ejecución de sumas de filas y columnas para una matriz 1x1 dada
 
     def test_calcular_sumas_matriz_unica_celda(self):
         matriz = [[7]]
@@ -28,6 +36,7 @@ class TestMatrizSumas(unittest.TestCase):
         self.assertEqual(filas_suma, [7])
         self.assertEqual(columnas_suma, [7])
 
+    # Comprobación de la correcta ejecución de sumas de filas y columnas para una matriz 5x5 aleatoria
     def test_calcular_sumas_matriz_cuadrada_random(self):
         matriz = generar_matriz(5)
         
